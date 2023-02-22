fa574ec1394d4b3defbb0d95e38d125532921d61
WilliamFridh <william.fridh@gmail.com>
2023-02-22 13:58:43 +0100

    final main.c

--- main.c
+++ main.c
@@ -3,22 +3,22 @@
 
 
 
-/*
-Settings & Data
-
-This section should hold the global settings and data
-that is'nt directly connected to either the game or menu.
+/**
+ * Settings & Data
+ * 
+ * This section should hold the global settings and data
+ * that is'nt directly connected to either the game or menu.
 */
-int screenCode = 1;
-int inGame = 1;
+int screen_code = 0;
+int in_game = 0;
 
 
 
-/*
-Main Function
-
-The main function is called on startup and will continue
-running infinite thanks to the while-loop.
+/**
+ * Main Function
+ * 
+ * The main function is called on startup and will continue
+ * running infinite thanks to the while-loop.
 */
 int main(void) {
 
@@ -37,72 +37,43 @@ int main(void) {
 
 
 
-/*
-Initilize Game Timer
-
-The game timer is used for keeping the game running at a certain speed.
-This timer will cause an interupt flag efter 100ms.
+/**
+ * Update Screen
+ * 
+ * Check if a game is running or not and call
+ * correct render function based on that.
 */
-void gameTimerInit(void) {
-	T2CON = 0x70;											 			// Stopping timer and setting the prescaler to 1/256
-	PR2 = ((80000000 / 256)/ 10);			 							// Set the period for the timer
-	TMR2 = 0;													 		// Tick to PR2
-}
-
-
-
-/*
-Start Game Timer
-*/
-void gameTimerStart(void) {
-	T2CONSET = 0x8000;													// Start timer
-}
-
-
-
-/*
-Stop Game Timer
-*/
-void gameTimerStop(void) {
-	T2CON = 0x70;											 			// Stopping timer and setting the prescaler to 1/256
-}
-
-
-
-/*
-Listen For Game Timer Tick
-
-This function is called repeatedly from
-the function main inside a inifinite loop.
-*/
-volatile int * porte = (volatile int *) 0xbf886110;						// Used for debugging ticks
-int timeoutCount = 0;													// A global counter used for slowing down the ticks
-int ledTime = 0;														// Used for debugging ticks 
-void listenForGameTimerTick(void) {
-	if (IFS(0) & 0x100) {												// Detect and interrupt flag
-		timeoutCount++;													// Increment global counter 
-		IFS(0) = 0;														// Clear flags
-		if (timeoutCount == 10) {										// If there as been 10 timeout event flags
-			timeoutCount = 0;
-			*porte = ledTime;											// Used for debugging ticks
-			ledTime++;													// Used for debugging ticks
-			/* ACTION STARTS */
-			// Add a function call here.
-			/* ACTION ENDS*/
-		}
-	}
-}
-
 void updateScreen(void) {
-	if (inGame) {
+	if (in_game) {
 		renderGame();
 	} else {
-		//renderMenu();
+		renderMenu();
 	}
 }
 
-/*
+
+
+/**
+ * Get Buttons
+*/
+int getButtons(void){
+    return (PORTD & 0xe0) >> 4; 
+}
+
+
+
+/**
+ * Listen For Inputs
+ * 
+ * This function calls getButtons, sorts
+ * this data and takes relevant action.
 */
 void listenForInput() {
-
+	int button_data = getButtons();
+	if (in_game) {
+		gameButtonTriggered(button_data);								// Send button data to the game button handler
+	} else {
+		menuButtonTriggered(button_data);								// Send button data to the menu button handler
+	}
 }
+
